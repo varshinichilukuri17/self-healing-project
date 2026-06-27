7a32ffc32311413f4258743a82d98e60d09e7506
varshinichilukuri123 <varshinichilukuri17@gmail.com>
2026-06-27 13:22:33 +0530

    Added self healing monitor

--- healer.py
+++ healer.py
@@ -1,3 +1,23 @@
-def auto_heal():
-    print("Self-Healing Action Triggered!")
-    print("Restarting application...")
+import os
+import requests
+import time
+
+def check_health():
+    try:
+        response = requests.get("http://localhost:5000/health")
+        if response.status_code == 200:
+            print("Application is healthy")
+        else:
+            print("Application unhealthy. Restarting...")
+            restart_container()
+    except:
+        print("Application down. Restarting...")
+        restart_container()
+
+def restart_container():
+    os.system("docker restart self-healing-container")
+    print("Container restarted")
+
+while True:
+    check_health()
+    time.sleep(10)
