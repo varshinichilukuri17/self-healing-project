0b4bf2eef48e93646791db623e8f0958fddc1d73
varshinichilukuri123 <varshinichilukuri17@gmail.com>
2026-06-30 13:01:01 +0530

    resolve CORS issue

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
 from flask import Flask
+import logging
 from prometheus_client import Counter, generate_latest
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:9090", "http://127.0.0.1:9090", "http://54.160.176.172:9090"])
+
+# Logging setup
+logging.basicConfig(
+    filename="app.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Prometheus metric
 REQUEST_COUNT = Counter("request_count", "Total Requests")
 
-
-@app.route("/")
+# Home route
+@app.route('/')
 def home():
     REQUEST_COUNT.inc()
     return "Self-Healing DevOps Project Running"
 
-
-@app.route("/health")
+# Health check route
+@app.route('/health')
 def health():
     REQUEST_COUNT.inc()
     return "Application is healthy"
 
-
-@app.route("/error")
+# Error simulation route
+@app.route('/error')
 def error():
     REQUEST_COUNT.inc()
-    return """
-    Root Cause Found: Application error detected!
-    Self-Healing Action Triggered! Restarting application...
-    """
+    try:
+        raise Exception("Application error detected!")
+    except Exception as e:
+        logging.error(str(e))
+        return "Root Cause Found: Application error detected!\nSelf-Healing Action Triggered!\nRestarting application..."
 
-
-@app.route("/metrics")
+# Metrics route for Prometheus
+@app.route('/metrics')
 def metrics():
     return generate_latest()
 
-
+# Main runner
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
